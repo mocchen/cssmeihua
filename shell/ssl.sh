836aaca51a0c871637c6c36f95a06fa838b31d1b
LeeMichael <87360548+mocchen@users.noreply.github.com>
2025-09-25 00:42:48 +0800

    Update ssl.sh

--- shell/ssl.sh
+++ shell/ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # OpenSSL批量端口扫描脚本（多线程版）- 单个目标版本
-# 用法: ./ssl_port_scanner.sh <目标> [选项]
+# 用法: .ssl.sh <目标> [选项]
 
 # 颜色定义
 RED='\033[0;31m'
@@ -33,7 +33,7 @@ usage() {
     echo "示例:"
     echo "  $0 example.com -p 443"
     echo "  $0 192.168.1.1 -p 80,443,8443 -j 24"
-    echo "  $0 183.2.133.238 -p 52000-52400 -j 24 --check-cert --output result.txt"
+    echo "  $0 192.168.1.1 -p 52000-52400 -j 24 --check-cert --output result.txt"
 }
 
 # IP验证函数
