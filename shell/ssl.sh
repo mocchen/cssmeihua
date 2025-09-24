3d779b12989f5b3f03674629c25e9929283a1908
LeeMichael <87360548+mocchen@users.noreply.github.com>
2025-09-25 01:53:20 +0800

    Update ssl.sh

--- shell/ssl.sh
+++ shell/ssl.sh
@@ -12,6 +12,18 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
+# 检查系统是否为 Debian
+if ! grep -qi "debian" /etc/os-release; then
+    echo -e "${RED}本脚本仅支持 Debian 系统，请在 Debian 系统上运行。${NC}"
+    exit 1
+fi
+
+# 检查用户是否为root
+if [ "$(id -u)" != "0" ]; then
+    echo -e "${RED}该脚本必须以root身份运行。${NC}"
+    exit 1
+fi
+
 # 显示用法信息
 usage() {
     echo "用法: $0 <目标> [选项]"
